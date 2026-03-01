82fa86bcc148cbd3f5f515f75cba231b6784fdb1
Foksif <knenrerp@gmail.com>
2026-03-01 13:04:17 +0700

    Initial

--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w"  -o ./build/gateway
--- go.mod
+++
@@ -1,5 +0,0 @@
-module svc-gateway
-
-go 1.24.4
-
-require gopkg.in/yaml.v3 v3.0.1 // indirect
